c8f31bc019da0400e106eb48a33576feefc5776d
ashu05808 <108938224+ashu05808@users.noreply.github.com>
2022-07-14 15:11:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+/*let js = 'amazing';
+console.log(40 + 20 - 5)
+
+console.log('Jonas');
+console.log('23');
+
+let firstName = "Jonas";
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+let jonas_maltida = 'JM';
+let $function = 27;
+
+let person = 'Jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'progammer';
+let myCurrentJob = 'teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+let lastName = 'Schemedtmann';
+
+console.log(firstName, ' ', lastName)*/
+
+/*let markMass = 78;
+let johnMass = 92;
+let markHeight = 1.69;
+let johnHeight = 1.95;
+
+markBMI = (markMass / (markMass * markMass))
+console.log(markMass / (markHeight * markHeight))
+
+johnBMI = (johnMass / (johnHeight * johnHeight))
+
+console.log(johnMass / (johnHeight * johnHeight))
+
+console.log(markBMI < johnBMI)
+
+const massMark = 95;
+const massJohn = 85;
+const heightJohn = 1.76;
+const heightMark = 1.88;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIJohn > BMIMark;
+
+console.log(BMIJohn, BMIMark, markHigherBMI)
+*/
+
+/*const age = 15;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+    console.log(`Sarah can start driving licence🚗`);
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait ${yearsLeft} another years`)
+}
+
+const birthYear = 1998;
+let century;
+
+if (birthYear <= 2000) {
+    century = 20;
+    console.log(century);
+
+} else {
+    century = 21;
+    console.log(century);
+}*/
+
+// const massMark = 95;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+// const heightMark = 1.88;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIJohn > BMIMark;
+
+// console.log(BMIJohn, BMIMark, markHigherBMI)
+
+// if (BMIJohn > BMIMark) {
+//     console.log(`John's BMI is higher than Mark's`);
+// }
+
+// console.log(`John's BMI is (${(BMIJohn)}) and Mark's BMI is (${(BMIMark)})`)
+
+// PROMPT FUNCTION*****
+
+// let favoriteNumber = prompt('Enter your Favourite Number');
+// console.log(favoriteNumber);
+
+// TERNERY OPERATOR *******
+
+// const age = 18;
+// age >= 18 ? console.log('One can drink wine🥂') : console.log('One can drink water💧')
+
+// if (age >= 18) {
+//     console.log('Wine 🥂')
+// } else {
+//     console.log('Water 💧')
+// }
+
+const billValue = 40;
+const tip = (billValue >= 50 && billValue <= 300 ? (billValue * 15) / 100 : (billValue * 20) / 100)
+
+console.log(`The bill was ${billValue}, tip was ${tip} and the total was ${billValue + tip}`);
